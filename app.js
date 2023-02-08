e6a85aa72ed4c9499175babcc3d820f1fa76cf5b
mttaskin <0mttaskin1@gmail.com>
2023-02-08 23:50:21 +0300

    js second part

--- app.js
+++ app.js
@@ -18,3 +18,25 @@ const passwordClick = () => {
         const c = String.fromCodePoint(i);
         lowerCase.push(c);
     };
+
+    let numbers = [];
+    for (let i = 48; i <= 57; i++) {
+        const d = String.fromCodePoint(i);
+        numbers.push(d);
+    };
+
+    const p = lowerCase.concat(upperCase);
+   
+    // 2 sembol
+    for (let i = 0; i < 2; i++) {
+        const q = Math.floor(Math.random() * sembol.length);
+        password.push(sembol[q]);
+    };
+
+    // 1 upperCase
+    const w = Math.floor(Math.random() * upperCase.length)
+    password.push(upperCase[w]);
+
+    // 1 lowerCase
+    const x = Math.floor(Math.random() * lowerCase.length)
+    password.push(lowerCase[x]);
